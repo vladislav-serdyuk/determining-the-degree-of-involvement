51dd00eafee6989d5224baef508e1b510cd6a3b2
Vlad <serdyuk2slava@gmail.com>
2026-02-10 20:48:27 +0300

    refactor: fix dockstring in face_detection.py and rename arg in video_stream.py

--- server_app/video_processing/video_stream.py
+++ server_app/video_processing/video_stream.py
@@ -10,21 +10,21 @@ class CaptureReadError(Exception): pass
 
 
 def process_video_stream(video_stream: cv2.VideoCapture,
-                         face_detector_and_emotion_recognizer: FaceAnalysisPipeline | None = None, *,
+                         face_analyze_pipeline: FaceAnalysisPipeline | None = None, *,
                          flip_h: bool = False):
     """
     Обрабатывает видеопоток, находя лица и распознавая эмоции
     :param video_stream: Видео поток
-    :param face_detector_and_emotion_recognizer: То, с помощью чего обрабатывается видеопоток
+    :param face_analyze_pipeline: То, с помощью чего обрабатывается видеопоток
     :param flip_h: Отзеркалить входящий видеопоток
     :return: Генератор, который возвращает обработанный кадр и эмоции. Формат: (image, [(emotion, confidence), ...])
     """
-    use_inner_models = face_detector_and_emotion_recognizer is None
+    use_inner_models = face_analyze_pipeline is None
     if use_inner_models:
         face_detector = FaceDetector(min_detection_confidence=0.5)
         emotion_recognizer = EmotionRecognizer(device='cuda' if torch.cuda.is_available() else 'cpu', window_size=15,
                                                confidence_threshold=0.55, ambiguity_threshold=0.15)
-        face_detector_and_emotion_recognizer = FaceAnalysisPipeline(face_detector, emotion_recognizer)
+        face_analyze_pipeline = FaceAnalysisPipeline(face_detector, emotion_recognizer)
 
     if not video_stream.isOpened():
         raise CaptureReadError('"video_stream" is not opened')
@@ -35,10 +35,10 @@ def process_video_stream(video_stream: cv2.VideoCapture,
                 raise CaptureReadError('Failed to get image from "video_stream"')
             if flip_h:
                 img = cv2.flip(img, 1)
-            new_img, emotions = face_detector_and_emotion_recognizer.analyze(img)
+            new_img, emotions = face_analyze_pipeline.analyze(img)
             yield new_img, emotions
     finally:
         if use_inner_models:
             face_detector.close()
             emotion_recognizer.reset()
-            del face_detector_and_emotion_recognizer
+            del face_analyze_pipeline
