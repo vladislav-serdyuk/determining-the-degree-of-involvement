89daeb344b2ae30ede9736068fb1bd70f291a71a
Vlad <137103197+vladislav-serdyuk@users.noreply.github.com>
2026-02-27 16:45:02 +0300

    fix room.py

--- backend/app/services/room.py
+++ backend/app/services/room.py
@@ -49,7 +49,6 @@ class Client:
     metrics: list[OneFaceMetricsAnalyzeResult] | None = None
     _frame_queue: asyncio.Queue | None = field(default=None, init=False, repr=False)
     _source_closed: asyncio.Event = field(default_factory=asyncio.Event, init=False, repr=False)
-
     def get_frame_queue(self) -> asyncio.Queue:
         """
         Получает очередь кадров для клиента.
